2174c42291db510a227827f242eb4c948e7da153
NownS <sksmsx147@gmail.com>
2020-03-06 09:59:24 -0500

    ISANG Network

---
+++ pcap_test[ISANG]/pcap_test/main.cpp
@@ -0,0 +1,139 @@
+#include <pcap.h>
+#include <stdio.h>
+#include <stdint.h>
+
+uint16_t uint16_LtoB(uint16_t value);
+
+#pragma pack(push, 1)
+struct ethheader{
+    uint8_t Dhost[6];
+    uint8_t Shost[6];
+    uint16_t Nlayer;
+};
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+struct ipheader{
+    uint8_t hlen:4, ver:4;
+    uint8_t DSF;
+    uint16_t Totlen;
+    uint16_t ID;
+    uint16_t Flag;
+    uint8_t TTL;
+    uint8_t Protocol;
+    uint16_t checksum;
+    uint8_t Sip[4];
+    uint8_t Dip[4];
+};
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+struct tcpheader{
+    uint16_t Sport;
+    uint16_t Dport;
+    uint32_t Seqnum;
+    uint32_t Acknum;
+    uint16_t hlenFlag;
+    uint16_t Size;
+    uint16_t checksum;
+    uint16_t Urgpointer;
+};
+#pragma pack(pop)
+
+struct application{
+    uint8_t value[16];
+};
+
+void usage() {
+  printf("syntax: pcap_test <interface>\n");
+  printf("sample: pcap_test wlan0\n");
+}
+
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    usage();
+    return -1;
+  }
+
+  char* dev = argv[1];
+  char errbuf[PCAP_ERRBUF_SIZE];
+  pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+  if (handle == NULL) {
+    fprintf(stderr, "couldn't open device %s: %s\n", dev, errbuf);
+    return -1;
+  }
+
+  while (true) {
+    struct pcap_pkthdr* header;
+    const u_char* packet;
+    int res = pcap_next_ex(handle, &header, &packet);
+    ethheader * ethernet = (ethheader*) packet;
+    printf("Eth header\n");
+    printf("Source Mac Addr [");
+    for(int i=0;i<6;i++)
+        printf("%02X:", ethernet->Shost[i]);
+    printf("\b]\t");
+    printf("Destination Mac Addr [");
+    for(int i=0;i<6;i++)
+        printf("%02X:", ethernet->Dhost[i]);
+    printf("\b]\n\n");
+    //-----------------------------------------eth header print
+    if(ethernet->Nlayer==0x0008)
+        packet=packet+14; // add offset
+    else
+    {
+        printf("%u bytes captured\n", header->caplen);
+        printf("--------------------------------------------------------------------------------------------------\n");
+        continue;
+    }
+    ipheader * ip = (ipheader*) packet;
+    printf("ip header\n");
+    printf("Source ip : %d.%d.%d.%d\t", ip->Sip[0],ip->Sip[1],ip->Sip[2],ip->Sip[3]);
+    printf("Destination ip : %d.%d.%d.%d\n\n", ip->Dip[0],ip->Dip[1],ip->Dip[2],ip->Dip[3]);
+    //------------------------------------------ip header print
+    if(ip->Protocol==6) packet=packet+sizeof(ipheader);
+    else
+    {
+        printf("%u bytes captured\n", header->caplen);
+        printf("--------------------------------------------------------------------------------------------------\n");
+        continue;
+    }
+    tcpheader * tcp = (tcpheader*) packet;
+    printf("tcp header \n");
+    printf("Source port : ");
+    printf("%d\t", uint16_LtoB(tcp->Sport));
+    printf("Destination port : %d\n\n",uint16_LtoB(tcp->Dport));
+    //------------------------------------------tcp header print
+    int paylen=(int)uint16_LtoB(ip->Totlen)-((int)ip->hlen)*4-(int)((tcp->hlenFlag>>4)&15)*4;
+    if(!paylen)
+    {
+        printf("%u bytes captured\n", header->caplen);
+        printf("--------------------------------------------------------------------------------------------------\n");
+        continue;
+    }
+    else
+    {
+        packet=packet+sizeof(tcpheader);
+    }
+    application * app = (application*) packet;
+    printf("application Layer value\n");
+    for(int i=0;i<16;i++) printf("%02X ",app->value[i]);
+    printf("\n\n");
+    if (res == 0) continue;
+    if (res == -1 || res == -2) break;
+    printf("%u bytes captured\n", header->caplen);
+    printf("--------------------------------------------------------------------------------------------------\n");
+  }
+
+  pcap_close(handle);
+  return 0;
+}
+
+uint16_t uint16_LtoB(uint16_t value)
+{
+    uint16_t temp;
+    temp=value;
+    value=value>>8;
+    temp=temp<<8;
+    return (uint16_t)(temp | value);
+}
